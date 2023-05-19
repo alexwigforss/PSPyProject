f529f97954fd96ab6d15169420b170d72f77073b
Alex Wigforss <alex.wigforss@gmail.com>
2023-05-19 15:12:01 +0200

    download imagelist

--- readimages.py
+++ readimages.py
@@ -1,1 +1,17 @@
-from main import test_images
+# https://www.scrapingbee.com/blog/download-image-python/
+import requests
+import shutil
+from main import test_images as ti
+
+def downloadImgs():
+    for e in ti:
+        url = ti[e]
+        fname = 'bilder/' + e.split()[0] + '_' + ti[e].split('/')[-1] + '.jpg'
+        res = requests.get(url, stream = False)
+
+        if res.status_code == 200:
+            with open(fname,'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+            print('Image sucessfully Downloaded: ',fname)
+        else:
+            print('Image Couldn\'t be retrieved: ',res.status_code)
