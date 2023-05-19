22a79b9b7e41abe7d09f8041e114dc1de57e6a8d
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-05-19 15:12:58 +0200

    Glömde spara innan förra pushen

--- collectimages.py
+++ collectimages.py
@@ -2,18 +2,37 @@ from main import test_links
 import requests
 from bs4 import BeautifulSoup
 
-# requests + beautiful soup
+# FÖR ATT HÄMTA BILD-URL FRÅN VARJE SIDA: requests + beautiful soup
 
-# set url
-urlfront = "https://www.impecta.se/sv/froer"
-urlpage = "https://www.impecta.se/froer/gronsaker/sommarmorot-nantes-2"
+start_url = "https://www.impecta.se"
+image_url = []
+# test-url för en artikelsida
+article_page = "https://www.impecta.se/froer/gronsaker/sommarmorot-nantes-2"
 
-# läs och lagra websidans hela html-kod
-front_r = requests.get(urlfront) 
-page_r = requests.get(urlpage) 
+# läs in websidans hela html-kod och lagra i soup med standard-parser
+page_html = requests.get(article_page) 
+soup = BeautifulSoup(page_html.text, 'html.parser')
 
-# läs med web scraper med standard-parser
-soup = BeautifulSoup(page_r.content, 'html.parser')
+def scrape_picture():
+    picture_code = soup.find(id="Bildkolumn") 
+    picture_links = picture_code.findAll('a')
+    for link in picture_links:
+        img = link.attrs['href']
+        if "_3.jpg" in img:
+            url = start_url+img
+            print(url)
+            image_url.append(url)
 
-result_link = soup.find(id="Bildkolumn") # för bildlänk. id = "ArtikelNamnfalt" för namn
-link_find = result_link.find_all('a')
+scrape_picture()
+
+def scrape_name():
+    text_code = soup.find(id="Faktakolumn")
+    name_tag = text_code.find('h1') #tag: <h1, attrs: id="", ignorera: <br-span> (innehållet skrivs ut genom .text)
+    print("name_code: " + str(name_tag))
+    name_str = str(name_tag.text)
+    print("name_str: " + name_str)
+
+scrape_name()
+
+#<a rev="/img/bilder/artiklar/zoom/89001_3.jpg?constrain=1&amp;w=850&amp;m=1651481439" OK as is
+#<a href="/bilder/artiklar/zoom/89001_3.jpg?m=1651481439" OK as is
