d60577c02bf0f6ea7b97c9959cd032b31bb7e785
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-05-22 11:54:45 +0200

    klass för information om varje planta

--- plantclass.py
+++ plantclass.py
@@ -1,12 +1,27 @@
-# klass för varje frösort med: Plantnamn (finns som key i dictionaryt)
+# klass för varje frösort med: 
+#       Plantnamn (finns som key i dictionaryt)
 #        Radavstånd (y)
  #       Plantavstånd (x)
-  #          Medelavstånd (initialisers som -1)
+  #      Medelavstånd
    #     Såtid (första)
-    #    Skördetid
+    #    Skördetid (dagar)
 #    Senare alternativ:
  #       Portionsmängd
   #      Perenn/ettårig
    #     (Grotid)
     #    (Grobarhet)
 
+class Plant:    
+    def __init__(self, name, x, y, plant_month, growth_days):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.plant_month = plant_month
+        self.growth_time = growth_days
+    # Area för radavstånd resp spridd sådd:
+        self.row_space = self.x * self.y
+        self.scatter_space = ((self.x + self.y) / 2) * ((self.x + self.y) / 2)
+
+# Importera dessa för att experimentera med matplotlib:
+TestPlant = Plant("morot", 4, 6, 6, 30)
+TestPlant2 = Plant("gurka", 2, 4, 6, 60)
