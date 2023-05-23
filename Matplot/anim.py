17766a68ecbc3d7c7440937fb6b1864d205042bd
Alex Wigforss <alex.wigforss@gmail.com>
2023-05-23 16:25:28 +0200

    copied anim.py to printgraph.py

--- Matplot/anim.py
+++ Matplot/anim.py
@@ -1,11 +1,3 @@
-# Skriv ut grafer månad för månad som ritar ut placering (utgå från spridd sådd) av rätt antal fröer, och när de ska skördas
-
-# Hitta frö med tidigast sådd, rita ut rätt antal av fröet i grafen 
-
-# Hitta nästa frö-sådd, rita ut rätt antal
-
-# markera skördetid 
-
 import numpy as np
 import matplotlib.pyplot as plt
 #import matplotlib.animation as animation
