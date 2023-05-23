c839ba778dec887c9b2c66da008ea80047d1c91b
Alex Wigforss <alex.wigforss@gmail.com>
2023-05-23 16:26:01 +0200

    almost no change

--- Labs/basicplot.py
+++ Labs/basicplot.py
@@ -3,7 +3,7 @@ import numpy as np
 xplist = [1,1,2,3,4]
 yplist = [1,2,2,3,4]
 
-plt.plot(xplist, yplist, 'o')
+plt.plot(xplist, yplist, '*')
 plt.show()
 
 #Two  lines to make our compiler able to draw:
